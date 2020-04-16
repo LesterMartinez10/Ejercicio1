09571967782c453dead7bb85699b2e10e9c4ef5c
Lester Orlando Martinez Hernández <201900062@colsanjose.edu.gt>
2020-04-16 12:15:08 -0600

    listo

--- calcular.php
+++ calcular.php
@@ -1,34 +1,31 @@
 <?php
 
+
    echo"<br>";  echo"<br>";  echo"<br>"; 
-   
-	 valor1=$_POST['n1'];
-     $valor2=$_POST['n2'];
-
-     $total=$valor1+$valor2;
-
-      echo "Tu resultado es:".$total;
-         
-
-	 $valor1=$_POST['n1'];
-     $valor2=$_POST['n2'];
-
-     $total=$valor1-$valor2;
-
-      echo "Tu resultado es:".$total;
-
-      $valor1=$_POST['n1'];
-      $valor2=$_POST['n2'];
-
-     $total=$valor1*$valor2;
-
-      echo "Tu resultado es:".$total;
+    
+     
+      
+	    $valor1=$_POST['n1'];
+        $valor2=$_POST['n2'];
+        $operacion=$_POST['operacion'];
+        $resultado= 0 ;
       
 
-      $valor1=$_POST['n1'];
-      $valor2=$_POST['n2'];
+        if ($operacion=="Suma")
+        {
+           $resultado= $valor1+$valor2;
+        }
+        elseif ($operacion=="Resta")
+        {  $resultado= $valor1-$valor2;
 
-     $total=$valor1/$valor2;
+         }
+         elseif ($operacion=="Multiplicacion")
+        {  $resultado= $valor1*$valor2;
 
-      echo "Tu resultado es:".$total;
+         }
+         elseif ($operacion=="Divicion")
+        {  $resultado= $valor1/$valor2;
+
+         }          
+         echo "El resultado es:".$resultado;
 ?>
